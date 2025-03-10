dbab65c72a30fc0125ba3db38d19110a7ad77f14
LucaDRF <lucadr.fiorenzano@gmail.com>
2025-03-09 23:47:54 -0300

    added routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { FavouriteContactListComponent } from './components/favourite-contact-list/favourite-contact-list.component';
+import { ContactRegistrationComponent } from './components/contact-registration/contact-registration.component';
+import { ContactEditingComponent } from './components/contact-editing/contact-editing.component';
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '**',
+    redirectTo: 'home',
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  { path: 'home', component: HomeComponent },
+  { path: 'edit', component: ContactEditingComponent },
+  { path: 'register', component: ContactRegistrationComponent },
+];

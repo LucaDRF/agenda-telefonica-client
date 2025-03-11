ccf0bc3b71975e90e9d8e0c1f40bc65dbdbb30e6
LucaDRF <lucadr.fiorenzano@gmail.com>
2025-03-10 23:27:23 -0300

    added style to home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
 import { ContactListComponent } from './contact-list/contact-list.component';
+import { NgClass } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [ContactListComponent],
+  imports: [ContactListComponent, NgClass],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  public isFilteredByFavourite: boolean = false;
 
+  constructor(public router: Router) {}
+
+  activateFilter() {
+    this.isFilteredByFavourite = !this.isFilteredByFavourite;
+  }
+
+  registerContact() {
+    this.router.navigate(['/register']);
+  }
 }

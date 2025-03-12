abc77b824ee68fba0bf785e656e1903142f56e51
LucaDRF <lucadr.fiorenzano@gmail.com>
2025-03-11 23:52:22 -0300

    added contact models

---
+++ src/app/models/contact-form.model.ts
@@ -0,0 +1,6 @@
+export interface ContactForm {
+  name: string;
+  email: string;
+  cellPhone: string;
+  telephone?: string;
+}
---
+++ src/app/models/contact.model.ts
@@ -0,0 +1,10 @@
+export interface Contact {
+  id: string;
+  name: string;
+  email: string;
+  cellPhone: string;
+  telephone: string;
+  favorite: boolean;
+  deleted: boolean;
+  createdAt: Date;
+}
